7f393ae5d56a033cabc5ed16379d31cb41a5a53a
Kbjareanrak <bins_aptx@hotmail.com>
2019-09-09 09:20:11 +0700

    Check4(hi)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import './App.css';
+import CharacterCard from './CharacterCard';
 
+const word = "Hello";
 class App extends Component {
+  
   render(){
     return (
-      <div className="App">
-        Hello! {"World"}
+      <div>
+        <CharacterCard value="h"/>
+        <CharacterCard value="i"/>
       </div>
     );
   }
