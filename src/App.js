34ff69cfce4197119eaa9396b67d9e88ad851596
Kbjareanrak <bins_aptx@hotmail.com>
2019-09-09 09:21:53 +0700

    Check4(hello)

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ class App extends Component {
   render(){
     return (
       <div>
-        <CharacterCard value="h"/>
-        <CharacterCard value="i"/>
+        {
+          Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+        }
       </div>
     );
   }
